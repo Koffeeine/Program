9e0e553a2ca5700ce8c5a1a1774d642b4abc9d0b
Koffeeine <147488095+Koffeeine@users.noreply.github.com>
2023-10-17 13:41:22 +0700

    Add files via upload

--- Homework/bai15_baithuchanh2.cpp
+++ Homework/bai15_baithuchanh2.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
-#include <cmath>
+#include<cmath>
 int main()
 {
 {
 	for(int x=1;x<=20;x++)
 		for(int y=1;y<=33;y++)		
 			for(int z=3;z<=100;z+=3)		
-				if ((x+y+z==100)&&(x*5+y*3+z/3==100))				
-					printf("x = %10d, y = %10d, z = %10d \n",x,y,z);			
+				if ((x+y+z==100)&&(x*5+y*3+z/3==100)){
+				std::cout<<"So trau dung la: "<<x<<" ";
+				std::cout<<"So trau nam la: "<<y<<" ";
+				std::cout<<"So trau gia la: "<<z<<" ";
+			}
 }
   return 0;
 }
